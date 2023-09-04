3c27ed6632ed889fd98a92a3d63ec9148f6b4de9
lutricuong <tcuong.lu@gmail.com>
2023-09-04 18:44:08 +0700

    done logic game

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const gameBoard = (() => {
     const cuong = player('cuong','X');
     const enemy = player('enemy','O');
     const tics = Array.from(document.querySelectorAll('.tic'));
+    const winner = document.querySelector('.winner');
     const count = [];
     const logic = [];
     const displayMarker = (tic) => {
@@ -15,15 +16,35 @@ const gameBoard = (() => {
                 tic.textContent = "X"
                 logic[tics.indexOf(tic)] = cuong;
                 count.push(cuong);
-                console.log(logic);
             }
         else {
             tic.textContent = "O"
-            logic[gameBoard.tics.indexOf(tic)] = enemy;
+            logic[tics.indexOf(tic)] = enemy;
             count.push(enemy);
         }
+        console.log(logic);
+        if (logic[0] == cuong && logic[0] == logic[1] && logic[1] == logic[2] ||
+            logic[3] == cuong && logic[3] == logic[4] && logic[4] == logic[5] ||
+            logic[6] == cuong && logic[6] == logic[7] && logic[7] == logic[8] ||
+            logic[0] == cuong && logic[0] == logic[3] && logic[3] == logic[6] ||
+            logic[1] == cuong && logic[1] == logic[4] && logic[4] == logic[7] ||
+            logic[2] == cuong && logic[2] == logic[5] && logic[5] == logic[8] ||
+            logic[0] == cuong && logic[0] == logic[4] && logic[4] == logic[8] ||
+            logic[2] == cuong && logic[2] == logic[4] && logic[4] == logic[6]) {
+            winner.textContent = "cuong win";
+        }
+        if (logic[0] == enemy && logic[0] == logic[1] && logic[1] == logic[2] ||
+            logic[3] == enemy && logic[3] == logic[4] && logic[4] == logic[5] ||
+            logic[6] == enemy && logic[6] == logic[7] && logic[7] == logic[8] ||
+            logic[0] == enemy && logic[0] == logic[3] && logic[3] == logic[6] ||
+            logic[1] == enemy && logic[1] == logic[4] && logic[4] == logic[7] ||
+            logic[2] == enemy && logic[2] == logic[5] && logic[5] == logic[8] ||
+            logic[0] == enemy && logic[0] == logic[4] && logic[4] == logic[8] ||
+            logic[2] == enemy && logic[2] == logic[4] && logic[4] == logic[6]) {
+            winner.textContent = "enemy win";
+        }
     }
-    return {tics, displayMarker};
+    const gameplay = tics.forEach((tic) => tic.addEventListener('click', displayMarker.bind(tics, tic)));
+    return {gameplay};
 })();
 
-gameBoard.tics.forEach((tic) => tic.addEventListener('click', gameBoard.displayMarker.bind(gameBoard.tics, tic)));
