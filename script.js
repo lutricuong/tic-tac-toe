fd7c21e61fe90c0662abaec2a88137a7cfb15085
lutricuong <tcuong.lu@gmail.com>
2023-09-14 21:51:25 +0700

    clean code js

--- script.js
+++ script.js
@@ -50,11 +50,15 @@ const gameboard = (() => {
     const getPlayer1 = () => player1;
     const getPlayer2 = () => player2;
 
-    const showDisplay = () => {
+    const displayPlayerCurrentFisrt = () => {
         if (player1.sign == 'X') {
             playerCurrent.textContent = player1.name + ' (' + player1.sign + ')' + ' turn';
         }
         else playerCurrent.textContent = player2.name + ' (' + player2.sign + ')' + ' turn';
+    }
+
+    const showDisplay = () => {
+        displayPlayerCurrentFisrt();
         player1boardName.textContent = player1.name + ' (' + player1.sign + ')';
         player2boardName.textContent = player2.name + ' (' + player2.sign + ')';
         player1boardPoint.textContent = '0';
@@ -65,7 +69,7 @@ const gameboard = (() => {
     buttonPickPlayer.addEventListener('click', showDialog);
     buttonConfirmPlayer.addEventListener('click', closeDialog);
 
-    return {buttonConfirmPlayer, playerCurrent, player1boardPoint, player2boardPoint, tiePoint, getPlayer1, getPlayer2}
+    return {buttonConfirmPlayer, playerCurrent, player1boardPoint, player2boardPoint, tiePoint, getPlayer1, getPlayer2, displayPlayerCurrentFisrt}
 })();
 
 const gameplay = (() => {
@@ -82,60 +86,36 @@ const gameplay = (() => {
             logic.pop();
         }
         round = 0;
-        return round;
     }
 
-    const displaySign = (tic) => {
+    const play = (tic) => {
         player1 = gameboard.getPlayer1();
         player2 = gameboard.getPlayer2();
         if (tic.textContent == '' && player1.sign == 'X') {
             if (round % 2 == 0) {
-                    gameboard.playerCurrent.textContent = player2.name + ' (' + player2.sign + ')' + ' turn';
-                    tic.textContent = 'X';
-                    logic[tics.indexOf(tic)] = player1.sign;
+                getSignDisplay(tic, player1, player2);
                 }
             else {
-                gameboard.playerCurrent.textContent = player1.name + ' (' + player1.sign + ')' + ' turn';
-                tic.textContent = 'O';
-                logic[tics.indexOf(tic)] = player2.sign;
+                getSignDisplay(tic, player2, player1);
             }
             round++;
         }
         else if (tic.textContent == '' && player2.sign == 'X') {
-            if (round % 2 == 1) {
-                    gameboard.playerCurrent.textContent = player1.name + ' (' + player1.sign + ')' + ' turn';
-                    tic.textContent = 'X';
-                    logic[tics.indexOf(tic)] = player2.sign;
+            if (round % 2 == 0) {
+                getSignDisplay(tic, player2, player1);
                 }
             else {
-                gameboard.playerCurrent.textContent = player2.name + ' (' + player2.sign + ')' + ' turn';
-                tic.textContent = 'O';
-                logic[tics.indexOf(tic)] = player1.sign;
+                getSignDisplay(tic, player1, player2);
             }
             round++;
         }
-        if (logic[0] == player1.sign && logic[0] == logic[1] && logic[1] == logic[2] ||
-            logic[3] == player1.sign && logic[3] == logic[4] && logic[4] == logic[5] ||
-            logic[6] == player1.sign && logic[6] == logic[7] && logic[7] == logic[8] ||
-            logic[0] == player1.sign && logic[0] == logic[3] && logic[3] == logic[6] ||
-            logic[1] == player1.sign && logic[1] == logic[4] && logic[4] == logic[7] ||
-            logic[2] == player1.sign && logic[2] == logic[5] && logic[5] == logic[8] ||
-            logic[0] == player1.sign && logic[0] == logic[4] && logic[4] == logic[8] ||
-            logic[2] == player1.sign && logic[2] == logic[4] && logic[4] == logic[6]) {
-            gameboard.player1boardPoint.textContent = Number(gameboard.player1boardPoint.textContent) + 1;
-            gameboard.playerCurrent.textContent = player1.name + ' (' + player1.sign + ')' + ' has won';
+
+        if (checkWinner(tics.indexOf(tic), player1)) {
+            getPoint(gameboard.player1boardPoint, player1);
             reset();
         }
-        else if (logic[0] == player2.sign && logic[0] == logic[1] && logic[1] == logic[2] ||
-            logic[3] == player2.sign && logic[3] == logic[4] && logic[4] == logic[5] ||
-            logic[6] == player2.sign && logic[6] == logic[7] && logic[7] == logic[8] ||
-            logic[0] == player2.sign && logic[0] == logic[3] && logic[3] == logic[6] ||
-            logic[1] == player2.sign && logic[1] == logic[4] && logic[4] == logic[7] ||
-            logic[2] == player2.sign && logic[2] == logic[5] && logic[5] == logic[8] ||
-            logic[0] == player2.sign && logic[0] == logic[4] && logic[4] == logic[8] ||
-            logic[2] == player2.sign && logic[2] == logic[4] && logic[4] == logic[6]) {
-            gameboard.player2boardPoint.textContent = Number(gameboard.player2boardPoint.textContent) + 1;
-            gameboard.playerCurrent.textContent = player2.name + ' (' + player2.sign + ')' + ' has won';
+        else if (checkWinner(tics.indexOf(tic), player2)) {
+            getPoint(gameboard.player2boardPoint, player2);
             reset();
         }
         else if (round === 9) {
@@ -145,15 +125,33 @@ const gameplay = (() => {
         }
     }
 
+    const getSignDisplay = (tic, player1, player2) => {
+        gameboard.playerCurrent.textContent = player2.name + ' (' + player2.sign + ')' + ' turn';
+        tic.textContent = player1.sign;
+        logic[tics.indexOf(tic)] = player1.sign;
+    }
+
+    const getPoint = (playerboardPoint, player) => {
+        playerboardPoint.textContent = Number(playerboardPoint.textContent) + 1;
+        gameboard.playerCurrent.textContent = player.name + ' (' + player.sign + ')' + ' has won';
+    }
+
+    const checkWinner = (index, player) => {
+        const winCondition = [
+            [0, 1, 2], [3, 4, 5], [6, 7 ,8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ]
+        return winCondition.filter((condition1) => condition1.includes(index))
+            .some((condition2) => condition2.every((condition3) => logic[condition3] === player.sign));
+    }
+
     const restart = () => {
-        if (player1.sign == 'X') {
-            gameboard.playerCurrent.textContent = player1.name + ' (' + player1.sign + ')' + ' turn';
-        }
-        else gameboard.playerCurrent.textContent = player2.name + ' (' + player2.sign + ')' + ' turn';
+        gameboard.displayPlayerCurrentFisrt();
         reset();
     }
 
-    tics.forEach((tic) => tic.addEventListener('click', displaySign.bind(tics, tic)));
+    tics.forEach((tic) => tic.addEventListener('click', play.bind(tics, tic)));
     buttonRestart.addEventListener('click', restart);
     return {reset};
 })();
